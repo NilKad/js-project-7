d70a83b7b7a8df069bf786ad91cf7fee65b89d5c
Max Siomak <pleomax250193@gmail.com>
2022-10-12 19:42:20 +0300

    fix bugs

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -2,5 +2,5 @@ document.body.onload = function () {
     setTimeout(function () {
         const preloader = document.querySelector('.preloader');
         preloader.classList.toggle('done')
-    }, 1000);
+    }, 500);
 }
